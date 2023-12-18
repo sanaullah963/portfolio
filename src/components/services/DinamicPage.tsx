c04a1a392e901a43fb74ff83809f8eb512de822a
sanaullah963 <allhera963@gmail.com>
2023-12-18 19:07:03 +0600

    services modify

--- src/components/services/DinamicPage.tsx
+++ src/components/services/DinamicPage.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import Container from "../Container";
 import Box from "../Box";
 import { services } from "@/constant/data";
+import BoxHeadding from "../BoxHeadding";
+import Services from "../Services";
 function DinamicPage({ data }: any) {
   return (
     <main>
       <Container>
         <Box className=" bg-lightBackground">
-          {services.map((i) => i.id == data && <p key={i.id}>{i.titel}</p>)}
+
+          {services.map((i) => i.id == data && (
+            <div key={i.id}>
+              <BoxHeadding headding={i.titel}/>
+              <p>{i.detail}</p>
+            </div>
+          ))}
+        </Box>
+        <Box className={'bg-lightBackground mt-10'}>
+          <Services/>
         </Box>
       </Container>
-      <button onClick={() => console.log(data)}>22clink</button>
     </main>
   );
 }
