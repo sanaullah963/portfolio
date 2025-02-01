490d58d953e1f8c1b8d968a824ac6a976c9f5f63
sanaullah963 <allhera963@gmail.com>
2025-02-02 01:42:12 +0600

    heirme page desgine

--- src/app/contact/hierme/page.tsx
+++ src/app/contact/hierme/page.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link';
 import React from 'react'
-
+import { FaCheckCircle } from "react-icons/fa";
 function page() {
   return (
-    <div>page hier me route</div>
+    <main className="w-full h-screen flex justify-center items-center ">
+      <div className="border border-green-400 rounded-lg px-5 py-10  flex flex-col items-center">
+        <span className='text-3xl text-green-400'><FaCheckCircle/></span>
+        <span>Your messag successfully send</span>
+        <Link className='underline text-green-400' href={'/contact'}>Back</Link>
+      </div>
+    </main>
   )
 }
 
