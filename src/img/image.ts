06909a657ebbd317ebcd06f29f8dee57ed9ec6f4
sanaullah963 <allhera963@gmail.com>
2025-02-23 21:27:58 +0600

    image optimization 2.0

--- src/img/image.ts
+++ src/img/image.ts
@@ -2,18 +2,17 @@ import logoColorfull from '@/img/logo-colorfull.png'
 import logoGray from '@/img/logo-gray.png'
 import banner from '@/img/banner.png'
 // protfolio image
-import protfolio1 from '@/img/protfolio/protfolio1.png';
-import protfolio2 from '@/img/protfolio/protfolio2.png';
-import protfolio3 from '@/img/protfolio/protfolio3.png';
-import protfolio4 from '@/img/protfolio/protfolio4.png';
-import protfolio5 from '@/img/protfolio/protfolio5.png';
-import protfolio6 from '@/img/protfolio/protfolio6.png';
+// import protfolio1 from '@/img/protfolio/protfolio1.png';
+// import protfolio2 from '@/img/protfolio/protfolio2.png';
+// import protfolio3 from '@/img/protfolio/protfolio3.png';
+// import protfolio4 from '@/img/protfolio/protfolio4.png';
+// import protfolio5 from '@/img/protfolio/protfolio5.png';
+// import protfolio6 from '@/img/protfolio/protfolio6.png';
 
 import image1 from '@/img/protfolio/image-1.webp';
 import image2 from '@/img/protfolio/image-2.webp';
 import image3 from '@/img/protfolio/image-3.webp';
 import image4 from '@/img/protfolio/image-4.webp';
-// import exampleImage from '@/img/protfolio/image-4.webp';
 import image5 from '@/img/protfolio/image-5.webp';
 import image6 from '@/img/protfolio/image-6.webp';
 import image7 from '@/img/protfolio/image-7.webp';
@@ -21,4 +20,4 @@ import image7 from '@/img/protfolio/image-7.webp';
 // contact page image
 import contactimage from '@/img/contact-image.png'
 
-export {logoColorfull,logoGray,banner,protfolio1,protfolio2,protfolio3,protfolio4,protfolio5,protfolio6,contactimage,image1,image2,image3,image4,image5,image6,image7}
+export {logoColorfull,logoGray,banner,contactimage,image1,image2,image3,image4,image5,image6,image7}
